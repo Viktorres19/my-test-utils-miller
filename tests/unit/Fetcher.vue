6f6c864e13086f4cb28e72a0ba8331895259aa74
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-11-22 11:55:06 +0200

    12 mocking http calls with jest

---
+++ tests/unit/Fetcher.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  mounted() {
+    axios.get('/')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
